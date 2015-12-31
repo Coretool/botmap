5af9bfcc4167ed8883cc719cbac4877026245ba8
Coretool <coretool@live.com>
2015-12-31 15:34:05 +0100

    fixed instal.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
+#install deps
 apt-get update
 apt-get install node.js
 apt-get install npm
 
-npm install botmap -g
-reboot
+#install botmap
+git clone https://coretool@bitbucket.org/coretool/botmap.git
+npm install -g
