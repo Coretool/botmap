efc41ca81dd26e88c66a1697b9b526e3d5b44503
tom Cat <tom Cat>
2016-01-01 14:54:31 +0100

    tre

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
 #install deps
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+apt-get install --yes nodejs
 apt-get update
-apt-get install node.js -y
 apt-get install npm -y
 
 #install botmap
+ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g
