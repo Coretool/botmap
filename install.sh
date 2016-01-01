17290cc45b19a7ba3ae9f0f2744db0b3671a2c01
tom Cat <tom Cat>
2016-01-01 15:08:55 +0100

    t

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -ex
 #install deps
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 apt-get update
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get install --yes nodejs
 apt-get install npm -y --fix-missing
 
 #install botmap
+nvm install 5.0
 ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g
+botmap about 
